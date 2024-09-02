fe78576b50454825e73bc20f93159b59747bb055
inf1g <inf1g@mail.ru>
2024-09-02 22:38:38 +0300

    Update devman_check.py
    исправил ошибку пустых значений

--- devman_check.py
+++ devman_check.py
@@ -4,17 +4,14 @@ from dotenv import load_dotenv
 from win11toast import toast
 
 
-def configure_keys(token):
+def configure_keys(token_name):
     load_dotenv()
-    key = os.getenv(token)
-    return key
+    return os.getenv(token_name)
 
 
 def get_response(token, status=None):
     url = "https://dvmn.org/api/long_polling/"
-    headers = {
-        'Authorization': f'Token {token}'
-    }
+    headers = {'Authorization': f'Token {token}'}
     response = requests.get(url, headers=headers, params=status)
     response.raise_for_status()
     return response.json()
@@ -22,33 +19,33 @@ def get_response(token, status=None):
 
 def check_status(response):
     if response['status'] == 'timeout':
-        return response['timestamp_to_request']
+        return response['timestamp_to_request'], None, None
     else:
-        return response['status'], response["new_attempts"][0]["is_negative"], response["new_attempts"][0]["lesson_url"]
+        attempt = response["new_attempts"][0]
+        return response['status'], attempt["is_negative"], attempt["lesson_url"]
 
 
 def alert(is_negative, lesson_url):
-    if is_negative:
-        answer = 'Нет'
-    else:
-        answer = 'Да'
-    toast('DEVMAN', f'Ответ по решению урока 🐦 Урок сдан?: {answer}',
-          audio='ms-winsoundevent:Notification.Looping.Alarm3',
-          on_click=lesson_url
-          )
+    answer = 'Нет' if is_negative else 'Да'
+    toast(
+        'DEVMAN',
+        f'Ответ по решению урока 🐦 Урок сдан?: {answer}',
+        audio='ms-winsoundevent:Notification.Looping.Alarm3',
+        on_click=lesson_url
+    )
 
 
 def main():
     token = configure_keys("DEVMAN_TOKEN")
-    status, is_negative, lesson_url = check_status(get_response(token))
+    status, is_negative, lesson_url = None, None, None
+
     while True:
+        response = get_response(token, {"timestamp": status} if status else None)
+        status, is_negative, lesson_url = check_status(response)
+
         if status == "found":
             alert(is_negative, lesson_url)
             break
-        else:
-            timestamp = {"timestamp": status}
-            status = check_status(get_response(token, timestamp))
-            continue
 
 
 if __name__ == '__main__':
